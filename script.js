009dde81c82dfbfe387b3387ef1e4ed96493332a
emmanuel2220 <pognonemmanuel95@gmail.com>
2025-03-13 20:40:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const text = "Moi c'est POGNON Emmanuel, Développeur web et agent de protection des systèmes informatiques";
-let index = 0;
-function typeWriter() {
-    if (index < text.length) {
-        document.getElementById("title").innerHTML += text.charAt(index);
-        index++;
-        setTimeout(typeWriter, 50);
-    }
-}
-window.onload = typeWriter;
+/*document.addEventListener("DOMContentLoaded", () => {
+    const title = document.querySelector("header h1");
+    let text = title.innerText;
+    title.innerText = "";
+    
+    text.split("").forEach((letter, i) => {
+        setTimeout(() => {
+            title.innerText += letter;
+        }, i * 100);
+    });
+});*/
+const textAnim = document.querySelector('h1');
+new Typewriter(textAnim, {
+    //deleteSpeed: 20
+})
+.typeString('Moi c\'est POGNON Emmanuel!') 
+.pauseFor(300)
+.typeString('<strong> <br> Développeur web  & agent de protection <br> des systèmes informatiques. </strong>')
+.start()
 
        
